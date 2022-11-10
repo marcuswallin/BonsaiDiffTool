9d9adaef2af1dd1f23b4be7ee53b667c957f0ff7
Marcus Wallin <marcus.wallin@arriver.com>
2022-11-10 16:51:00 +0100

    First commit

---
+++ cmake-3.25.0-rc4-tutorial-source/Step2_build/MathFunctions/CMakeFiles/MathFunction.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MathFunction.dir/mysqrt.cxx.o"
+  "libMathFunction.pdb"
+  "libMathFunction.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MathFunction.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+//
+// Created by marcus.wallin on 11/10/22.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World" << "\n";
+    return 1;
+}
