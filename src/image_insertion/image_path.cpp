e3ea90724cdb58d66bb4c20949d42b48a885acf5
Marcus Wallin <marcus.wallin93@outlook.com>
2023-01-05 21:20:26 +0100

    Progress on image insetion bash parsing

--- src/image_insertion/image_path.cpp
+++ src/image_insertion/image_path.cpp
@@ -8,14 +8,15 @@ using namespace std;
 bool insert_image(const std::string &tree_name, const std::string &file_path, const std::string &output_dir)
 {
     auto input_path = filesystem::path(file_path);
-    if (input_path.extension() != ".png")
+    if (input_path.extension() == ".png" || input_path.extension() == ".jpg")
+    {
+        auto tree_dir = filesystem::path(output_dir) / tree_name;
+        filesystem::create_directory(tree_dir);
+        auto output_path = tree_dir / input_path.filename();
+        return filesystem::copy_file(input_path, output_path);
+    }
+    else
     {
         throw WrongFileTypeException{};
     }
-    auto tree_dir = filesystem::path(output_dir) / tree_name;
-    filesystem::create_directory(tree_dir);
-    auto output_path = tree_dir / input_path.filename();
-    
-    return filesystem::copy_file(input_path, output_path);
 }
-
--- src/main.cpp
+++ src/main.cpp
@@ -3,33 +3,44 @@
 #include <image_path.h>
 #include <cxxopts.hpp>
 
-#define IMAGE_PATH "/d/MyStuff/bonsai-images"
+#define IMAGE_PATH "/mnt/d/bonsai-images"
+
+namespace bonsai
+{
+    void run_insert(int argc, char *argv[])
+    {
+        cxxopts::Options options("insert", "some info here");
+        options.add_options()("t,tree-name", "The name of the tree to add an image to", cxxopts::value<std::string>())("i,input", "Path to the input image", cxxopts::value<std::string>())("h,help", "Print usage");
+
+        auto parsed_options = options.parse(argc, argv);
+
+        if (parsed_options.count("help"))
+        {
+            std::cout << options.help() << std::endl;
+        }
+
+        if (!parsed_options.count("tree-name") || !parsed_options.count("input"))
+        {
+            std::cout << "Need two input arguments hlo"
+                      << "\n";
+            return;
+        }
+
+        insert_image(parsed_options["tree-name"].as<std::string>(), parsed_options["input"].as<std::string>(), IMAGE_PATH);
+    }
+}
 
 int main(int argc, char *argv[])
 {
-
-    cxxopts::Options options("dog", "some info here");
-    options.add_options()
-        ("n,number", "Show line numbers") 
-        ("E,show-ends", "Show line endings")
-        ("h,help", "Print usage")
-    ;
-    
-    auto result = options.parse(argc, argv);
-
-    if (result.count("help"))
+    if (argc < 2)
     {
-      std::cout << options.help() << std::endl;
-      exit(0);
-    }
-
-    if(argc != 3)
-    {
-        std::cout << "Need two input arguments hello" << "\n";
+        std::cout << "need inputs" << std::endl;
         return -1;
     }
-    insert_image((std::string) argv[1], (std::string) argv[2], IMAGE_PATH);
 
+    if ((std::string)argv[1] == "insert")
+    {
+        bonsai::run_insert(argc, argv);
+    }
     return 1;
 }
-
